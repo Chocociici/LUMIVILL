24db555f67e925a5c773cab50987faf68c00be94
Chocociici <foxangel2607@gmail.com>
2026-05-26 01:01:21 +0400

    Fix settings path: app0.app0.settings

--- app0/app0/asgi.py
+++ app0/app0/asgi.py
@@ -4,7 +4,7 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from app0.chat.routing import websocket_urlpatterns
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app0.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app0.app0.settings')
 
 application = ProtocolTypeRouter({
     'http': get_asgi_application(),
