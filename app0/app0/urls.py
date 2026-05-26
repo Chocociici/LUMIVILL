9872bb0c3cb07a6323a464916c88b993a4e4be40
Chocociici <foxangel2607@gmail.com>
2026-05-26 16:12:32 +0400

    Fix views import path

--- app0/app0/urls.py
+++ app0/app0/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from app0 import views
+from app0.app0 import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
